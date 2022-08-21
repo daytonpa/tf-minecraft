99e64e1f73379d408d20e0d1b40e20f34551cb28
Patrick Dayton <daytonpa@gmail.com>
2022-08-21 09:23:16 -0400

    save point

--- stack_tier_03/launch_configs.tf
+++ stack_tier_03/launch_configs.tf
@@ -29,11 +29,35 @@ data "aws_ami" "amazon" {
   }
 }
 
-resource "aws_launch_configuration" "as_conf" {
-  name = "minecraft-server-ubuntu-"
-  name_prefix   = "terraform-lc-example-"
+# Launch configuration
+data "template_file" "minecraft_server_user_data" {
+  template = "../templates/user_data/minecraft_server.sh.tmpl"
+  vars = {
+    aws_region = ''
+    chef_environment = ''
+    chef_role = ''
+    s3_chef_repo_bucket = ''
+    s3_chef_repo_path = ''
+  }
+}
+
+resource "aws_launch_configuration" "minecraft_server" {
+  name = "minecraft-server-ubuntu-${local.timestamp}"
+
+  iam_instance_profile = "minecraft-server-profile"
+  key_name = data.terraform_remote_state.stack_01.kms_key_ebs
+
   image_id      = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+
+  security_groups = [
+    data.terraform_remote_state.stack_02.sg_minecraft_base_id,
+    data.terraform_remote_state.stack_02.sg_minecraft_server_id
+  ]
+
+  ebs_optimized = true
+
+  user_data = 
 
   lifecycle {
     create_before_destroy = true
