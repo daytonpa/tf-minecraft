299b840252b73aa4f61002790f95b6daa6a4657d
Patrick Dayton <daytonpa@gmail.com>
2022-08-30 18:58:42 -0400

    ssh and kms

--- stack_tier_02/outputs.tf
+++ stack_tier_02/outputs.tf
@@ -7,3 +7,15 @@ output "sg_minecraft_server_id" {
 output "sg_minecraft_bastion_id" {
   value = module.minecraft_network_and_firewalls.sg_minecraft_bastion_id
 }
+output "kms_key_ebs_id" {
+  value = aws_kms_key.ebs.id
+}
+output "kms_key_ssm_id" {
+  value = aws_kms_key.ssm.id
+}
+output "ssh_key_name" {
+  value = aws_key_pair.ssh.key_name
+}
+output "ssh_key_id" {
+  value = aws_key_pair.ssh.id
+}
