e3026231568d0e92707a55e95cc00941dd08db93
Patrick Dayton <daytonpa@gmail.com>
2022-09-01 17:48:20 -0400

    dummy backup policy

--- stack_tier_01/outputs.tf
+++ stack_tier_01/outputs.tf
@@ -5,6 +5,20 @@ output "kms_s3_id" {
   value = aws_kms_key.minecraft_data_bucket.id
 }
 
+output "iam_arns" {
+  value = {
+    "profiles": {
+      "minecraft-bastion-profile": "${aws_iam_instance_profile.minecraft_bastion.arn}",
+      "minecraft-server-profile": "${aws_iam_instance_profile.minecraft_server.arn}",
+    },
+    "roles": {
+      "minecraft-backups-role": "${aws_iam_role.minecraft_backups.arn}",
+      "minecraft-bastion-role": "${aws_iam_role.minecraft_bastion.arn}",
+      "minecraft-server-role": "${aws_iam_role.minecraft_server.arn}",
+    }
+  }
+}
+
 output "s3_data_bucket_name" {
   value = aws_s3_bucket.minecraft_data.id
 }
